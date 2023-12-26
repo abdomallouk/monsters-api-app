02d11aa087cd91d05673ed0cebb1aeb81c448a5c
abdomallouk <abdelbassetmallouk@gmail.com>
2023-12-26 14:54:05 +0100

    adding typescript to the Monsters app

---
+++ src/components/search-box/search-box.component.tsx
@@ -0,0 +1,35 @@
+import { ChangeEvent, ChangeEventHandler } from "react";
+import "./search-box.styles.css";
+
+// interface ISearchBoxProps {
+//   className: string;
+//   placeholder?: string;
+
+// }
+
+// interface ISearchBoxProps {
+//   onChangeHandler: (a: string) => void
+
+// }
+
+type SearchBoxProps = {
+  className: string;
+  placeholder?: string;
+  // onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+  onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
+};
+
+const SearchBox = ({
+  className,
+  placeholder,
+  onChangeHandler,
+}: SearchBoxProps) => (
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+
+export default SearchBox;
